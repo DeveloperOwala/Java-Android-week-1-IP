80fb7f54d6b3d3c0c3b95953ecbaed49a9d11469
DeveloperOwala <otienoerick46@gmail.com>
2021-10-10 21:36:11 +0300

    creating View object in main class

--- app/src/main/java/com/example/mymathgaming/MainActivity.java
+++ app/src/main/java/com/example/mymathgaming/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         buttonPlay.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
 
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(intent);
