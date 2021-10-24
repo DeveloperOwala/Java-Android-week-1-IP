bef3234ec80bddf9c02acd24f5462467c25deb8e
DeveloperOwala <otienoerick46@gmail.com>
2021-10-24 16:44:51 +0300

    android search widget

--- app/src/main/java/com/example/mymathgaming/ui/LoginActivity.java
+++ app/src/main/java/com/example/mymathgaming/ui/LoginActivity.java
@@ -13,12 +13,12 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mymathgaming.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.moringaschool.newsupdates.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
